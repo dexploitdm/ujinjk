7605db2d8672f05b9203a94c0c0e333cb17d1726
Дмитрий Паршаков <knutdmit@yandex.ru>
2019-10-26 13:42:11 +0500

    full header + banner

--- home.php
+++ home.php
@@ -7,7 +7,7 @@ get_header(); ?>
     <main>
         <?php get_template_part( 'components/banboxes/ban-home'); ?>
 
-
+        <?php get_template_part( 'components/about'); ?>
 
     </main>
 <?php get_footer(); ?>
